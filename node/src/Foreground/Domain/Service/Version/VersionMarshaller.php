d736991269f7f73b23d0e76da53631a240b09dbc
Baptiste CONTRERAS <38988658+BaptisteContreras@users.noreply.github.com>
2025-08-30 23:16:22 +0200

    feat(Foreground): work on version marshaller

--- node/src/Foreground/Domain/Service/Version/VersionMarshaller.php
+++ node/src/Foreground/Domain/Service/Version/VersionMarshaller.php
@@ -7,15 +7,28 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class VersionMarshaller
 {
-    // public function transformToString(AsyncVectorClock $vectorClock): string;
-    public function __construct(private SerializerInterface $serializer)
-    {
-    }
+    /**
+     * @param VectorClock|array<VectorClock> $version
+     */
+     public function transformToString(VectorClock|array $version): string
+     {
+         if ($version instanceof VectorClock) {
+             $version = [$version];
+         }
+
+         return base64_encode(json_encode(array_map(fn(VectorClock $vectorClock) => $vectorClock->getVector(), $version), JSON_THROW_ON_ERROR));
+     }
+
 
     public function transformFromString(string $stringVersion): VectorClock
     {
-        $rawTimestamps = json_decode(base64_decode($stringVersion), true, 512, JSON_THROW_ON_ERROR);
+        /** @var array<int, array<string, positive-int>> $rawVectors */
+        $rawVectors = json_decode(base64_decode($stringVersion), true, 512, JSON_THROW_ON_ERROR);
 
-        dd($rawTimestamps);
+        $clocks = array_map(fn(array $vector) => new VectorClock($vector), $rawVectors);
+
+        // TODO a little bit of validation ?
+
+        return VectorClock::merge(...$clocks);
     }
 }
---
+++ node/tests/Unitary/Foreground/Domain/Service/VersionMarshallerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Tests\Unitary\Foreground\Domain\Service;
+
+use App\Foreground\Domain\Service\Version\VersionMarshaller;
+use App\Shared\Domain\Model\Versioning\VectorClock;
+use PHPUnit\Framework\TestCase;
+
+class VersionMarshallerTest extends TestCase
+{
+    public final const NODE_1 = 'node1';
+    public final const NODE_2 = 'node2';
+
+    private VersionMarshaller $versionMarshaller;
+
+    protected function setUp(): void
+    {
+        $this->versionMarshaller = new VersionMarshaller();
+    }
+
+    public function getTestTransformFromStringSuccessCases(): \Generator
+    {
+        yield 'Valid string and empty' => [
+            'W10=',
+            []
+        ];
+        yield 'Valid string and one node' => [
+            'W3sibm9kZTEiOjF9XQ==',
+            [self::NODE_1 => 1]
+        ];
+        yield 'Valid string and multiple nodes' => [
+            'W3sibm9kZTEiOjF9LHsibm9kZTEiOjIsIm5vZGUyIjozfSx7Im5vZGUyIjoyfV0=',
+            [self::NODE_1 => 2, self::NODE_2 => 3]
+        ];
+    }
+
+
+    /**
+     * @dataProvider getTestTransformFromStringSuccessCases
+     */
+    public function testTransformFromString(string $stringVersion, array $expectedVectorResult): void
+    {
+        $resultClock = $this->versionMarshaller->transformFromString($stringVersion);
+
+        $resultVector = $resultClock->getVector();
+
+
+        self::assertCount(count($expectedVectorResult), $resultVector);
+        self::assertEmpty(array_diff_key($expectedVectorResult, $resultVector));
+
+        foreach ($expectedVectorResult as $expectedResultKey => $expectedResultValue) {
+            self::assertEquals($expectedResultValue, $resultVector[$expectedResultKey]);
+        }
+    }
+}
