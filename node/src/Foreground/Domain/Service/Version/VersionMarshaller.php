b7956ab89531e46aa8d6dc8074447071a250c305
Baptiste CONTRERAS <38988658+BaptisteContreras@users.noreply.github.com>
2025-08-30 23:23:14 +0200

    build: add node Makefile

--- node/src/Foreground/Domain/Service/Version/VersionMarshaller.php
+++ node/src/Foreground/Domain/Service/Version/VersionMarshaller.php
@@ -3,29 +3,27 @@
 namespace App\Foreground\Domain\Service\Version;
 
 use App\Shared\Domain\Model\Versioning\VectorClock;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class VersionMarshaller
 {
     /**
      * @param VectorClock|array<VectorClock> $version
      */
-     public function transformToString(VectorClock|array $version): string
-     {
-         if ($version instanceof VectorClock) {
-             $version = [$version];
-         }
+    public function transformToString(VectorClock|array $version): string
+    {
+        if ($version instanceof VectorClock) {
+            $version = [$version];
+        }
 
-         return base64_encode(json_encode(array_map(fn(VectorClock $vectorClock) => $vectorClock->getVector(), $version), JSON_THROW_ON_ERROR));
-     }
-
+        return base64_encode(json_encode(array_map(fn (VectorClock $vectorClock) => $vectorClock->getVector(), $version), JSON_THROW_ON_ERROR));
+    }
 
     public function transformFromString(string $stringVersion): VectorClock
     {
         /** @var array<int, array<string, positive-int>> $rawVectors */
         $rawVectors = json_decode(base64_decode($stringVersion), true, 512, JSON_THROW_ON_ERROR);
 
-        $clocks = array_map(fn(array $vector) => new VectorClock($vector), $rawVectors);
+        $clocks = array_map(fn (array $vector) => new VectorClock($vector), $rawVectors);
 
         // TODO a little bit of validation ?
 
